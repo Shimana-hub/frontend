5c53a8f8c0f2a535ffa121de5993d9ccc782daac
Shimana Yagba <shimanayagba@gmail.com>
2025-09-22 20:53:22 +0100

    Initial Next.js + Bootstrap setup

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Button from "react-bootstrap/Button";
+
+export default function Home() {
+  return (
+    <div className="container mt-5">
+      <h1 className="text-center text-primary">Worker-Client Platform</h1>
+      <Button variant="success">Bootstrap Working!</Button>
+    </div>
+  );
+}
